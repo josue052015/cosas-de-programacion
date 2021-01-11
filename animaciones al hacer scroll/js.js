dcb38e5d5cda05cccabccd1e35fe0b0aac356e4d
josue052015 <76499684+josue052015@users.noreply.github.com>
2021-01-11 03:19:07 +0100

    cambios

--- animaciones al hacer scroll/js.js
+++ animaciones al hacer scroll/js.js
@@ -5,5 +5,10 @@ window.addEventListener('scroll',function(){
     let tamañopantalla = window.innerHeight/3;
     if(posicion < tamañopantalla){
         animacion.style.animation = 'mover 1s ease-out'
+        animacion.style.opacity = 100
+    }
+    if(posicion > tamañopantalla){
+      /*  animacion.style.animation = 'mover 1s ease-out'*/
+        animacion.style.opacity = 0
     }
 })
