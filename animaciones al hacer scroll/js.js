a488bc534ca68ee69123b90abe2f84e2637162d0
josue052015 <76499684+josue052015@users.noreply.github.com>
2021-01-13 04:02:26 +0100

    no term

--- animaciones al hacer scroll/js.js
+++ animaciones al hacer scroll/js.js
@@ -2,13 +2,13 @@ window.addEventListener('scroll',function(){
     let animacion = document.getElementById('animado');
     let posicion = animacion.getBoundingClientRect().top
     console.log(posicion);
-    let tamañopantalla = window.innerHeight/3;
+    let tamañopantalla = window.innerHeight/1.3;
     if(posicion < tamañopantalla){
         animacion.style.animation = 'mover 1s ease-out'
         animacion.style.opacity = 100
     }
     
-    let tamañopantalla2 = window.innerHeight - 55;
+    let tamañopantalla2 = window.innerHeight - 75;
     if(posicion > tamañopantalla2){
         animacion.style.animation = null
         animacion.style.opacity = 0
